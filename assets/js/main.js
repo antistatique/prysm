cf6440be711a2b01bffe82c1ab7d9249a88c6b0b
Toni Fisler <mail@tonifisler.ch>
2015-04-24 13:20:26 +0200

    layout page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
 'user strict';
 
 (function(){
+  var text_max = 250;
+  var string = ' caractères restants.';
+  $('#textarea-help').html(text_max + string);
 
+  $('#fieldihhlst').keyup(function() {
+    var text_length = $('#fieldihhlst').val().length;
+    var text_remaining = text_max - text_length;
+
+    $('#textarea-help').html(text_remaining + string);
+  });
+
+  // FOCUS ME
+  $('.focuspoint').focusPoint();
 }());
--- tasks/images.js
+++ tasks/images.js
@@ -3,10 +3,22 @@ module.exports = function(gulp, $, config) {
  /**
   * Copy images
   */
-  gulp.task('img', function() {
+  gulp.task('img-optim', function() {
     return gulp.src(config.images)
       .pipe($.size({title: "IMAGES"}))
-      .pipe(gulp.dest(config.app.buildpath + 'img'));
+      .pipe(gulp.dest(config.build + 'img'));
   });
+
+  /**
+   * Copy svg
+   */
+   gulp.task('svg-optim', function() {
+     return gulp.src(config.svg)
+       .pipe($.svgo())
+       .pipe($.size({title: "SVG"}))
+       .pipe(gulp.dest(config.build + 'svg'));
+   });
+
+   gulp.task('img', ['img-optim', 'svg-optim']);
 
 }
