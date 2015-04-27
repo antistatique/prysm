e679b8ffc5a951e5dc19acb4ee15ce7db6510df9
Toni Fisler <mail@tonifisler.ch>
2015-04-27 12:02:33 +0200

    initial commit gh-pages

---
+++ build/js/main.js
@@ -0,0 +1,50 @@
+'user strict';
+
+(function(){
+  var text_max = 250;
+  var string = ' caractères restants.';
+  $('#textarea-help').html(text_max + string);
+
+  $('#fieldihhlst').keyup(function() {
+    var text_length = $('#fieldihhlst').val().length;
+    var text_remaining = text_max - text_length;
+
+    $('#textarea-help').html(text_remaining + string);
+  });
+
+  // FOCUS ME. -Footer image
+  $('.focuspoint').focusPoint();
+
+  // STICK ME. -Navbar content
+  $("#sidebar-nav").stick_in_parent({
+    parent: '#container-wrapper'
+  });
+
+  var svgTriggered = false;
+  $('body').on('activate.bs.scrollspy', function (item) {
+    if ($(window).width() > 767 ) {
+      if ($(item.target).hasClass('svg-trigger') && !svgTriggered) {
+        svgTriggered = true;
+        $('.svg-container').addClass('get-together');
+      } else if (!$(item.target).hasClass('svg-trigger') && svgTriggered) {
+        svgTriggered = false;
+        $('.svg-container').removeClass('get-together');
+      }
+    } else {
+      $('.svg-container').addClass('get-together');
+    }
+  });
+
+  // scroll events
+  var header_height = $('header.header').height() * 2 / 3;
+  $(window).scroll(function () {
+    if ($(window).width() > 767 ) {
+      var scroll_position = $(window).scrollTop() - 150;
+      var diff = header_height - scroll_position;
+      var opacity = scroll_position / header_height > 1 ? 1 : scroll_position / header_height;
+      $('#navbar-main').css('opacity', (1 - opacity));
+      $('#sidebar-nav').css('opacity', opacity);
+    }
+  });
+
+}());
